b265f752f94837c912a64cc54141ae7ddc6967ea
Jacob Quinn Shenker <jacob@shenker.net>
2018-03-29 05:47:40 -0400

    Trenchwise/positionwise mapping functions.

--- paulssonlab/src/paulssonlab/shenker/matriarch/trench_segmentation.py
+++ paulssonlab/src/paulssonlab/shenker/matriarch/trench_segmentation.py
@@ -1,5 +1,9 @@
 import numpy as np
+import pandas as pd
+import zarr
 import skimage
+from trench_detection import get_img_limits
+from util import tqdm_auto
 
 
 def hessian_eigenvalues(img):
@@ -91,8 +95,81 @@ def get_trench_bbox(trench_points, trench_idx, x_lim, y_lim, overlap=0):
     return bounding_box(points)
 
 
+def trenchwise_apply(
+    img_stack,
+    trench_set_points,
+    trench_idx,
+    func,
+    channel_slice=slice(None),
+    time_slice=slice(None),
+):
+    x_lim, y_lim = get_img_limits(img_stack.shape[-2:])
+    ul, lr = get_trench_bbox(trench_set_points, trench_idx, x_lim, y_lim)
+    if len(img_stack.shape) == 4:
+        thumb_stack = (
+            img_stack.oindex if isinstance(img_stack, zarr.Array) else img_stack
+        )[channel_slice, time_slice, ul[1] : lr[1], ul[0] : lr[0]]
+    else:
+        thumb_stack = img_stack[..., ul[1] : lr[1], ul[0] : lr[0]]
+    res = func(thumb_stack)
+    return res
+
+
+def trenchwise_map(
+    img_stack, trench_points, func, progress_bar=tqdm_auto, preload=True, **kwargs
+):
+    if preload:
+        img_stack = (
+            img_stack.oindex if isinstance(img_stack, zarr.Array) else img_stack
+        )[
+            kwargs.get("channel_slice", slice(None)),
+            kwargs.get("time_slice", slice(None)),
+            :,
+            :,
+        ]
+        del kwargs["channel_slice"]
+        del kwargs["time_slice"]
+    obj = {
+        trench_set_idx: pd.concat(
+            {
+                trench_idx: trenchwise_apply(
+                    img_stack, trench_set_points, trench_idx, func, **kwargs
+                )
+                for trench_idx in progress_bar(range(len(trench_set_points[0])))
+            },
+            axis=1,
+        )
+        for trench_set_idx, trench_set_points in progress_bar(trench_points.items())
+    }
+    df = pd.concat(obj, axis=1)
+    df.columns.set_names("trench_set_idx", level=0, inplace=True)
+    df.columns.set_names("trench_idx", level=1, inplace=True)
+    return df
+
+
+def positionwise_trenchwise_map(
+    img_group, trench_points_pos, func, positions=None, progress_bar=tqdm_auto, **kwargs
+):
+    if positions is None:
+        positions = trench_points_pos.keys()
+    obj = {
+        pos: trenchwise_map(
+            img_group[pos],
+            trench_points_pos[pos],
+            func,
+            progress_bar=lambda x: x,
+            **kwargs,
+        )
+        for pos in progress_bar(positions)
+    }
+    df = pd.concat(obj, axis=1)
+    df.columns.set_names("position", level=0, inplace=True)
+    return df
+
+
 def image_sharpness(img):
     # FROM: https://stackoverflow.com/questions/7765810/is-there-a-way-to-detect-if-an-image-is-blurry/7767755#7767755
+    # TODO: vectorize?? normalize?? verify against some kind of ground truth?
     img_blurred = skimage.filters.gaussian(img, 1)
     img_lofg = skimage.filters.laplace(img_blurred)
     return np.percentile(img_lofg, 99.9)
