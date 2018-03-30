d641621719c3b9d2c99a7d6a92d32983c00b57ea
Jacob Quinn Shenker <jacob@shenker.net>
2018-03-29 21:02:47 -0400

    Watershed trench segmentation was calculating distance transform, doing a bunch of other things that make no sense. Fixed. Works decently but not perfectly.

--- paulssonlab/src/paulssonlab/shenker/matriarch/trench_segmentation/watershed.py
+++ paulssonlab/src/paulssonlab/shenker/matriarch/trench_segmentation/watershed.py
@@ -5,6 +5,7 @@ import skimage
 import skimage.morphology
 from .core import hessian_eigenvalues
 from ui import RevImage
+from util import repeat_apply
 
 
 def _trench_img(img):
@@ -57,40 +58,47 @@ def segment_trench(img, threshold=4e-2, diagnostics=None):
     bin_img = skimage.morphology.binary_dilation(bin_img)
     # bin_img = skimage.morphology.binary_erosion(bin_img)
     # bin_img = skimage.morphology.binary_dilation(bin_img)
+    bin_img = skimage.segmentation.clear_border(bin_img, in_place=True)
     if diagnostics is not None:
         # diagnostics['threshold_value'] = threshold_value
         diagnostics["bin_img"] = _trench_img(bin_img)
-    labeled_img = skimage.morphology.label(bin_img)  # (1-bin_img)
-    if diagnostics is not None:
-        diagnostics["labeled_img"] = _trench_img(labeled_img)
-    bg_label = scipy.stats.mode(labeled_img.flat[labeled_img.flat != 0]).mode[0]
-    if diagnostics is not None:
-        diagnostics["bg_label"] = bg_label
-    all_labels = (labeled_img * (labeled_img != bg_label)) != 0
-    if diagnostics is not None:
-        diagnostics["all_labels"] = _trench_img(all_labels)
-    distances = scipy.ndimage.distance_transform_edt(all_labels)
-    if diagnostics is not None:
-        diagnostics["distances"] = _trench_img(distances)
-    # res = skimage.feature.peak_local_max(distance_transform, indices=False, footprint=np.ones((3, 3)), labels=labeled_img)
-    # res2 = skimage.feature.peak_local_max(distance_transform, indices=False, min_distance=20)
-    #     eroded_distances = repeat_apply(skimage.morphology.erosion, 10)(distances)
-    #     if diagnostics is not None:
-    #         diagnostics['eroded_distances'] = _trench_img(eroded_distances)
-    labeled_distances = skimage.morphology.label(
-        distances != 0 + (labeled_img == bg_label)
+    # labeled_img = skimage.morphology.label(bin_img)#(1-bin_img)
+    # if diagnostics is not None:
+    #     diagnostics['labeled_img'] = _trench_img(labeled_img)
+    # if diagnostics is not None:
+    #     diagnostics['all_labels'] = _trench_img(all_labels)
+    # distances = scipy.ndimage.distance_transform_edt(all_labels)
+    # if diagnostics is not None:
+    #     diagnostics['distances'] = _trench_img(distances)
+    # eroded_distances = repeat_apply(skimage.morphology.erosion, 10)(distances)
+    # if diagnostics is not None:
+    #     diagnostics['eroded_distances'] = _trench_img(eroded_distances)
+    # background = all_labels != (labeled_img != 0)
+    # if diagnostics is not None:
+    #     diagnostics['background'] = _trench_img(background)
+    # labeled_distances = skimage.morphology.label(distances != 0 + background)
+    # if diagnostics is not None:
+    #     diagnostics['labeled_distances'] = _trench_img(labeled_distances)
+    # labeled_distances_clean = skimage.morphology.remove_small_objects(labeled_distances, 20)
+    # if diagnostics is not None:
+    #     diagnostics['labeled_distances_clean'] = _trench_img(labeled_distances_clean)
+    clean_seeds = skimage.morphology.label(
+        skimage.morphology.remove_small_objects(bin_img, 20)
     )
     if diagnostics is not None:
-        diagnostics["labeled_distances"] = _trench_img(labeled_distances)
-    bg_label2 = scipy.stats.mode(labeled_distances.flat != 0).mode[0]
-    labeled_distances_clean = skimage.morphology.remove_small_objects(
-        labeled_distances, 20
-    )
+        diagnostics["clean_seeds"] = _trench_img(clean_seeds)
+    mask = img > skimage.filters.threshold_otsu(img)
     if diagnostics is not None:
-        diagnostics["labeled_distances_clean"] = _trench_img(labeled_distances_clean)
-    watershed_labels = skimage.morphology.watershed(img_k2, labeled_distances_clean)
-    watershed_labels[watershed_labels == bg_label2] = 0  # set background to 0
-    # watershed_labels_rescaled = skimage.transform.rescale(watershed_labels, 1/6, anti_aliasing=False)
+        diagnostics["mask"] = _trench_img(mask)
+    watershed_labels = skimage.morphology.watershed(
+        img_k2, clean_seeds, mask=mask, watershed_line=True
+    )
     if diagnostics is not None:
         diagnostics["watershed_labels"] = _trench_img(watershed_labels)
-    return watershed_labels  # _rescaled
+    watershed_labels_eroded = repeat_apply(skimage.morphology.erosion, 1)(
+        watershed_labels
+    )
+    # watershed_labels_rescaled = skimage.transform.rescale(watershed_labels, 1/6, anti_aliasing=False)
+    if diagnostics is not None:
+        diagnostics["watershed_labels_eroded"] = _trench_img(watershed_labels_eroded)
+    return watershed_labels_eroded  # _rescaled
