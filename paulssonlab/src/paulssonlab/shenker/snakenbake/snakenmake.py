707e7cd229e28e83bea705814b3ad9bcec516437
Jacob Quinn Shenker <jacob@shenker.net>
2018-05-02 22:11:04 -0400

    Can make multiple snakes on a chip and specify how to split up the lanes.

--- paulssonlab/src/paulssonlab/shenker/snakenbake/snakenmake.py
+++ paulssonlab/src/paulssonlab/shenker/snakenbake/snakenmake.py
@@ -2,12 +2,14 @@ import numpy as np
 import gdspy as g
 import click
 from functools import partial
+import numbers
 
 DEFAULT_DIMS = (23e3, 13e3)
 
 Cell = partial(g.Cell, exclude_from_current=True)
 
 # TODO: encode parameters in cell names
+# TODO: break out func for each cell, use memoize decorator to reuse cells from multiple chips
 
 
 def outline(dims, thickness=0.15e3):
@@ -19,6 +21,15 @@ def outline(dims, thickness=0.15e3):
 
 # def chip(design_func, dims=DEFAULT_DIMS, name=None):
 #     cell = Cell('Chip_{}'.format(name) if name else 'Chip')
+
+# TODO: add snake gap using multiple snake calls, put gap between them on chip
+
+
+def make_odd(number):
+    if number % 2 == 0:
+        return number - 1
+    else:
+        return number
 
 
 def snake(
@@ -42,9 +53,28 @@ def snake(
         [dims[0] - 2 * margin - outer_snake_turn_radius, feeding_channel_width]
     )
     lane_height = feeding_channel_width + 2 * effective_trench_length
-    num_lanes = int((dims[1] - 2 * margin) // lane_height)
-    if num_lanes % 2 == 0:
-        num_lanes -= 1  # ensure odd
+    max_lanes = int((dims[1] - 2 * margin) // lane_height)
+    if split is None:
+        split = 1
+    if isinstance(split, numbers.Integral):
+        lanes_per_snake = make_odd(max_lanes // split)
+        remainder_lanes_per_snake = make_odd(max_lanes - (split - 1) * lanes_per_snake)
+        split = (lanes_per_snake,) * (split - 1) + (remainder_lanes_per_snake,)
+    else:
+        if isinstance(split[0], numbers.Integral):
+            if sum(split) > max_lanes:
+                raise Exception(
+                    "total lanes desired {} is greater than maximum number of lanes {}".format(
+                        sum(split), max_lanes
+                    )
+                )
+            if np.any(np.array(split) % 2 == 0):
+                raise Exception("number of lanes per snake must be odd")
+        else:
+            raise Exception(
+                "snake splitting spec must be a integer or sequence of integers"
+            )
+    num_lanes = sum(split)
     trench_xs = np.arange(
         -(lane_fc_dims[0] - trench_width) / 2,
         (lane_fc_dims[0] + trench_width) / 2,
@@ -64,9 +94,6 @@ def snake(
     snake_cell = Cell("Snake")
     lane_fc = g.Rectangle(-lane_fc_dims / 2, lane_fc_dims / 2)
     lane_cell.add(lane_fc)
-    lane_ys = np.linspace(-last_lane_y, last_lane_y, num_lanes)
-    for y in lane_ys:
-        snake_cell.add(g.CellReference(lane_cell, (0, y)))
     bend_cell = Cell("Feeding Channel Bend")
     bend = g.Round(
         (0, 0),
@@ -76,24 +103,46 @@ def snake(
     )
     bend = g.slice(bend, 0, 0)
     bend_cell.add(bend[1])
-    for y in lane_ys[1::2]:
-        snake_cell.add(
-            g.CellReference(bend_cell, (lane_fc_dims[0] / 2, y + lane_height / 2))
-        )
-        snake_cell.add(
-            g.CellReference(
-                bend_cell, (-lane_fc_dims[0] / 2, y - lane_height / 2), rotation=180
-            )
-        )
     port_x = dims[0] / 2 - lane_fc_dims[0] / 2 - port_radius - port_margin
     port = g.Round((port_x, 0), port_radius, number_of_points=500)
     port_fc = g.Rectangle((0, -lane_fc_dims[1] / 2), (port_x, lane_fc_dims[1] / 2))
     port_cell.add(port)
     port_cell.add(port_fc)
-    snake_cell.add(
-        g.CellReference(port_cell, (-lane_fc_dims[0] / 2, lane_ys[-1]), rotation=180)
+    lane_ys = np.linspace(last_lane_y, -last_lane_y, num_lanes)
+    split_cum = np.concatenate(((0,), np.cumsum(split)))
+    right_port_lanes = split_cum[1:] - 1
+    left_port_lanes = split_cum[:-1]
+    right_bend_lanes = np.concatenate(
+        [
+            np.arange(start, stop - 1, 2)
+            for start, stop in zip(split_cum[:-1], split_cum[1:])
+        ]
     )
-    snake_cell.add(g.CellReference(port_cell, (lane_fc_dims[0] / 2, lane_ys[0])))
+    left_bend_lanes = right_bend_lanes + 1
+    for y in lane_ys:
+        snake_cell.add(g.CellReference(lane_cell, (0, y)))
+    for lane in right_bend_lanes:
+        snake_cell.add(
+            g.CellReference(
+                bend_cell, (lane_fc_dims[0] / 2, lane_ys[lane] - lane_height / 2)
+            )
+        )
+    for lane in left_bend_lanes:
+        snake_cell.add(
+            g.CellReference(
+                bend_cell,
+                (-lane_fc_dims[0] / 2, lane_ys[lane] - lane_height / 2),
+                rotation=180,
+            )
+        )
+    for lane in right_port_lanes:
+        snake_cell.add(g.CellReference(port_cell, (lane_fc_dims[0] / 2, lane_ys[lane])))
+    for lane in left_port_lanes:
+        snake_cell.add(
+            g.CellReference(
+                port_cell, (-lane_fc_dims[0] / 2, lane_ys[lane]), rotation=180
+            )
+        )
     return (snake_cell, lane_cell, bend_cell, trench_cell, port_cell), metadata
 
 
