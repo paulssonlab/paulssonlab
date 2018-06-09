072eb2776f23f4dab1ca5915367b9280014c11fb
Jacob Quinn Shenker <jacob@shenker.net>
2018-06-09 01:01:29 -0400

    Forgot to remove these when moved to diagnostics.py

--- paulssonlab/src/paulssonlab/shenker/matriarch/util.py
+++ paulssonlab/src/paulssonlab/shenker/matriarch/util.py
@@ -46,13 +46,6 @@ def iterate_get_collection_value(obj, level):
     return obj
 
 
-def extract_diagnostics_singleton(diagnostics, keys):
-    return wrap_dict_values(
-        drop_dict_nones({k: iterate_getattr(v, keys) for k, v in diagnostics.items()}),
-        ".".join(keys),
-    )
-
-
 def wrap_dict_values(obj, label):
     return {k: {label: v} for k, v in obj.items()}
 
@@ -70,61 +63,6 @@ def find_all(a_str, sub):
             return
         yield start
         start += 1  # find overlapping matches (otherwise += len(sub))
-
-
-def wrap_diagnostics(func):
-    @wraps(func)
-    def wrapper(*args, **kwargs):
-        diag = tree()
-        return (func(*args, **{"diagnostics": diag, **kwargs}), diag)
-
-    return wrapper
-
-
-# @wrapt.decorator
-# def wrap_diagnostics(func, instance, args, kwargs):
-#     diag = tree()
-#     return (func(*args, **{'diagnostics': diag, **kwargs}), diag)
-
-
-def diagnostics_to_dataframe(diagnostics):
-    d = flatten_dict(
-        diagnostics, predicate=lambda _, x: not isinstance(x, hv.ViewableElement)
-    )
-    df = pd.DataFrame.from_dict({0: d}, orient="index")
-    return df
-
-
-def expand_diagnostics_by_label(df, label="label_", keep_all=True):
-    data = {}
-    column_to_label_num = {}
-    column_to_flattened_name = {}
-    label_nums = set()
-    for column in df.columns:
-        column_parts = column.split(".")
-        if column_parts[0].startswith(label):
-            label_num = int(column_parts[0][len(label) :])
-            column_to_label_num[column] = label_num
-            column_to_flattened_name[column] = ".".join(column_parts[1:])
-            label_nums.add(label_num)
-    for idx, row in df.iterrows():
-        d = defaultdict(dict)
-        for column in df.columns:
-            if column in column_to_label_num:
-                label_num = column_to_label_num[column]
-                d[label_num][column_to_flattened_name[column]] = row[column]
-            else:
-                if keep_all:
-                    for label_num in label_nums:
-                        d[label_num][column] = row[column]
-        # data.extend(d.values())
-        for label_num, dd in d.items():
-            data[(idx, label_num)] = dd
-    df2 = pd.DataFrame.from_dict(data, orient="index")
-    # parent_index = pd.MultiIndex(df.index)
-    # df2.index.rename(parent_index.names, level=parent_index.levels, inplace=True)
-    df2.index.rename([df.index.name, label[:-1]], inplace=True)
-    return df2
 
 
 def drop_constant_columns(df):
