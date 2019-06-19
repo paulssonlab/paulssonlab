cbd7a3801cea2192d8020bda82a924ff6e47d4d3
Jacob Quinn Shenker <jacob@shenker.net>
2019-06-19 14:17:16 -0400

    New version of watershed segmentation, slight modification from molecule-counting codebase.

--- paulssonlab/src/paulssonlab/shenker/matriarch/matriarch/trench_segmentation/watershed.py
+++ paulssonlab/src/paulssonlab/shenker/matriarch/matriarch/trench_segmentation/watershed.py
@@ -22,141 +22,77 @@ def normalize_trench_intensity(img):
     return img / normalization
 
 
-def segment_trench_old(img, threshold=4e-2, diagnostics=None):
+def segment(img, dtype=np.uint16, diagnostics=None):
+    img = skimage.img_as_float(img)
     if diagnostics is not None:
-        diagnostics["img"] = _trench_img(img)
-    img = normalize_trench_intensity(uniformize_trench_intensity(img))
-    if diagnostics is not None:
-        diagnostics["img_normalized"] = _trench_img(img)
-    img_scaled = skimage.transform.pyramid_expand(img, upscale=2)
-    if diagnostics is not None:
-        diagnostics["img_scaled"] = _trench_img(img_scaled)
-    img_blurred = skimage.filters.gaussian(img_scaled, 0.5)
-    if diagnostics is not None:
-        diagnostics["img_blurred"] = _trench_img(img_scaled)
-    img_k1 = hessian_eigenvalues(img_blurred)[0]
-    if diagnostics is not None:
-        diagnostics["img_k1"] = _trench_img(img_k1)
-    bin_img = (-threshold < img_k1) & (img_k1 < threshold)
-    bin_img = skimage.morphology.binary_erosion(bin_img)
-    bin_img = skimage.morphology.binary_dilation(bin_img)
-    bin_img = skimage.segmentation.clear_border(bin_img, in_place=True)
-    if diagnostics is not None:
-        diagnostics["bin_img"] = _trench_img(bin_img)
-    clean_seeds = skimage.morphology.label(
-        skimage.morphology.remove_small_objects(bin_img, 20)
-    )
-    if diagnostics is not None:
-        diagnostics["clean_seeds"] = _trench_img(clean_seeds)
-    mask = img > skimage.filters.threshold_otsu(img)
-    if diagnostics is not None:
-        diagnostics["mask"] = _trench_img(mask)
-    watershed_labels = skimage.morphology.watershed(
-        img_k1, clean_seeds, mask=mask, watershed_line=True
-    )
-    if diagnostics is not None:
-        diagnostics["watershed_labels"] = _trench_img(watershed_labels)
-    watershed_labels_eroded = repeat_apply(skimage.morphology.erosion, 1)(
-        watershed_labels
-    )
-    if diagnostics is not None:
-        diagnostics["watershed_labels_eroded"] = _trench_img(watershed_labels_eroded)
-    return watershed_labels_eroded.astype(np.uint8)
-
-
-def segment_trench(img, diagnostics=None):
-    if diagnostics is not None:
-        diagnostics["img"] = _trench_img(img)
-    img = normalize_trench_intensity(uniformize_trench_intensity(img))
-    if diagnostics is not None:
-        diagnostics["img_normalized"] = _trench_img(img)
-    # img_scaled = skimage.transform.pyramid_expand(img, upscale=2,
-    #                                              multichannel=False)
-    # if diagnostics is not None:
-    #    diagnostics['img_scaled'] = _trench_img(img_scaled)
+        diagnostics["img"] = RevImage(img)
+    ##img_scaled = skimage.transform.pyramid_expand(img, upscale=2,
+    ##                                              multichannel=False)
+    ##if diagnostics is not None:
+    ##    diagnostics['img_scaled'] = RevImage(img_scaled)
     img_blurred = skimage.filters.gaussian(img, 0.5)
     if diagnostics is not None:
-        diagnostics["img_blurred"] = _trench_img(img_blurred)
-    img_k1 = hessian_eigenvalues(img_blurred)[0]
+        diagnostics["img_blurred"] = RevImage(img_blurred)
     mask = img_blurred > skimage.filters.threshold_otsu(img_blurred)
+    mask = skimage.morphology.remove_small_objects(mask, 5)
     if diagnostics is not None:
-        diagnostics["mask"] = _trench_img(mask)
+        diagnostics["mask"] = RevImage(mask)
+    mask_labels = skimage.morphology.label(mask)
+    # img_normalized = normalize_componentwise(img, mask_labels)
+    # if diagnostics is not None:
+    #    diagnostics['img_normalized'] = RevImage(img_normalized)
+    img_k1 = hessian_eigenvalues(img)[0]
+    # TODO: necessary?
+    img_k1 -= img_k1.min()
+    img_k1 /= img_k1.max()
     if diagnostics is not None:
-        diagnostics["img_k1"] = _trench_img(img_k1)
-    img_k1_frangi = skimage.filters.frangi(img_k1, scale_range=(1, 3), scale_step=0.5)
+        diagnostics["img_k1"] = RevImage(img_k1)
+    # img_k1_frangi = skimage.filters.frangi(img_k1, sigmas=np.arange(0.1,1.5,0.5))#, scale_range=(1,3), scale_step=0.5)
+    img_k1_frangi = skimage.filters.frangi(
+        img_k1, sigmas=np.arange(0.2, 3, 0.2)
+    )  # , scale_range=(1,3), scale_step=0.5)
     if diagnostics is not None:
-        diagnostics["img_k1_frangi"] = _trench_img(img_k1_frangi)
-    img_thresh = img_k1_frangi > skimage.filters.threshold_otsu(img_k1_frangi)
-    # bin_img = (-threshold < img_k1) & (img_k1 < threshold)
-    # bin_img = skimage.morphology.binary_erosion(bin_img)
-    # bin_img = skimage.morphology.binary_dilation(bin_img)
-    # bin_img = skimage.segmentation.clear_border(bin_img, in_place=True)
-    if diagnostics is not None:
-        diagnostics["img_thresh"] = _trench_img(img_thresh)
-    clean_seeds = skimage.morphology.label(
-        skimage.morphology.remove_small_objects(img_thresh * mask, 5)
+        diagnostics["img_k1_frangi"] = RevImage(img_k1_frangi)
+    # # TODO: necessary?
+    img_k1_frangi -= img_k1_frangi.min()
+    img_k1_frangi /= img_k1_frangi.max()
+    # img_k1_frangi_uint = skimage.img_as_uint(img_k1_frangi)
+    # if diagnostics is not None:
+    #     diagnostics['img_k1_frangi_uint'] = RevImage(img_k1_frangi_uint)
+    # selem = skimage.morphology.disk(20)
+    # img_k1_frangi_thresh = skimage.filters.rank.otsu(img_k1_frangi_uint, selem)
+    img_k1_frangi_thresh = skimage.filters.threshold_local(
+        img_k1_frangi, block_size=23, mode="nearest"
     )
     if diagnostics is not None:
-        diagnostics["clean_seeds"] = _trench_img(clean_seeds)
+        diagnostics["img_k1_frangi_thresh"] = RevImage(img_k1_frangi_thresh)
+    # img_k1_frangi_thresh_blurred = skimage.filters.gaussian(img_k1_frangi_thresh, 0)
+    # if diagnostics is not None:
+    #     diagnostics['img_k1_frangi_thresh_blurred'] = RevImage(img_k1_frangi_thresh_blurred)
+    # img_thresh = img_k1_frangi > img_k1_frangi_thresh_blurred
+    img_thresh = img_k1_frangi > img_k1_frangi_thresh
+    # img_thresh = img_k1_frangi > skimage.filters.threshold_otsu(img_k1_frangi)
+    if diagnostics is not None:
+        diagnostics["img_thresh"] = RevImage(img_thresh)
+    img_thresh_masked = img_thresh * mask
+    if diagnostics is not None:
+        diagnostics["img_thresh_masked"] = RevImage(img_thresh_masked)
+    # img_thresh_eroded = repeat_apply(skimage.morphology.erosion, 0)(img_thresh_masked)
+    # if diagnostics is not None:
+    #     diagnostics['img_thresh_eroded'] = RevImage(img_thresh_eroded)
+    # clean_seeds = skimage.morphology.label(skimage.morphology.remove_small_objects(img_thresh_eroded, 5))
+    clean_seeds = skimage.morphology.label(
+        skimage.morphology.remove_small_objects(img_thresh_masked, 5)
+    )
+    if diagnostics is not None:
+        diagnostics["clean_seeds"] = RevImage(clean_seeds)
     watershed_labels = skimage.morphology.watershed(
         img_k1, clean_seeds, mask=mask, watershed_line=False, compactness=0.01
     )
-    watershed_labels = watershed_labels.astype(np.uint8)
+    watershed_labels = watershed_labels.astype(dtype)
     if diagnostics is not None:
-        diagnostics["watershed_labels"] = _trench_img(watershed_labels)
-    # watershed_labels_eroded = repeat_apply(skimage.morphology.erosion, 1)(watershed_labels)
-    # if diagnostics is not None:
-    #    diagnostics['watershed_labels_eroded'] = _trench_img(watershed_labels_eroded)
-    # return watershed_labels_eroded.astype(np.uint8)
-    return watershed_labels
-
-
-def segment_trench2(img, diagnostics=None):
-    if diagnostics is not None:
-        diagnostics["img"] = _trench_img(img)
-    img = normalize_trench_intensity(uniformize_trench_intensity(img))
-    if diagnostics is not None:
-        diagnostics["img_normalized"] = _trench_img(img)
-    # img_scaled = skimage.transform.pyramid_expand(img, upscale=2,
-    #                                              multichannel=False)
-    # if diagnostics is not None:
-    #    diagnostics['img_scaled'] = _trench_img(img_scaled)
-    img_blurred = skimage.filters.gaussian(img, 0.5)
-    if diagnostics is not None:
-        diagnostics["img_blurred"] = _trench_img(img_blurred)
-    img_k1 = hessian_eigenvalues(img_blurred)[0]
-    mask = img_blurred > skimage.filters.threshold_otsu(img_blurred)
-    if diagnostics is not None:
-        diagnostics["mask"] = _trench_img(mask)
-    if diagnostics is not None:
-        diagnostics["img_k1"] = _trench_img(img_k1)
-    img_k1_frangi = skimage.filters.frangi(img_k1, scale_range=(1, 3), scale_step=0.5)
-    if diagnostics is not None:
-        diagnostics["img_k1_frangi"] = _trench_img(img_k1_frangi)
-    if diagnostics is not None:
-        diagnostics["img_k1_frangi_blurred"] = _trench_img(img_k1_frangi_blurred)
-    img_thresh = img_k1_frangi_blurred > skimage.filters.threshold_otsu(
-        img_k1_frangi_blurred
-    )
-    # bin_img = (-threshold < img_k1) & (img_k1 < threshold)
-    # bin_img = skimage.morphology.binary_erosion(bin_img)
-    # bin_img = skimage.morphology.binary_dilation(bin_img)
-    # bin_img = skimage.segmentation.clear_border(bin_img, in_place=True)
-    if diagnostics is not None:
-        diagnostics["img_thresh"] = _trench_img(img_thresh)
-    clean_seeds = skimage.morphology.label(
-        skimage.morphology.remove_small_objects(img_thresh * mask, 5)
-    )
-    if diagnostics is not None:
-        diagnostics["clean_seeds"] = _trench_img(clean_seeds)
-    watershed_labels = skimage.morphology.watershed(
-        img_k1, clean_seeds, mask=mask, watershed_line=False, compactness=0.01
-    )
-    watershed_labels = watershed_labels.astype(np.uint8)
-    if diagnostics is not None:
-        diagnostics["watershed_labels"] = _trench_img(watershed_labels)
-    # watershed_labels_eroded = repeat_apply(skimage.morphology.erosion, 1)(watershed_labels)
-    # if diagnostics is not None:
-    #    diagnostics['watershed_labels_eroded'] = _trench_img(watershed_labels_eroded)
-    # return watershed_labels_eroded.astype(np.uint8)
+        diagnostics["watershed_labels"] = RevImage(watershed_labels)
+        diagnostics["watershed_labels_permuted"] = RevImage(
+            permute_labels(watershed_labels)
+        )
     return watershed_labels
