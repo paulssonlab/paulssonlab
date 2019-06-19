2591bd97dc8b2d4348d66b6cbc9619aa85fcc37b
Jacob Quinn Shenker <jacob@shenker.net>
2019-06-19 14:16:51 -0400

    Make RevImage work with new bokeh.

--- paulssonlab/src/paulssonlab/shenker/matriarch/matriarch/ui.py
+++ paulssonlab/src/paulssonlab/shenker/matriarch/matriarch/ui.py
@@ -45,15 +45,16 @@ def RevImage(img, **kwargs):
 
 
 def _RevImage(cls, img, **kwargs):
+    # TODO: bug in holoviews/bokeh aspect ratio handling, you need data_aspect=(height/width)**2 instead of 1
+    # TODO: I think datashader is doing this correctly, but the original Image display screws up the frame dimensions
+    # return cls(img[::-1], bounds=(0,0,img.shape[1],img.shape[0])).options(invert_yaxis=True, data_aspect=(img.shape[1]/img.shape[0])**2, width=400)#, responsive=False)
     return cls(img[::-1], bounds=(0, 0, img.shape[1], img.shape[0])).options(
-        invert_yaxis=True
-    )
+        aspect=img.shape[1] / img.shape[0], width=250
+    )  # , responsive=False)
 
 
 def _trench_img(img):
-    return RevImage(img.T).options(
-        backend="bokeh", height=120, width=400
-    )  # .options(height=10, width=100)#.options(height=int(img.shape[1]*0.6), width=int(img.shape[0]*0.4))
+    return RevImage(img.T)
 
 
 def RevRGB(img, **kwargs):
