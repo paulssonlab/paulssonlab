1261b2cbac1865b2b350d9adc7b509b1e957832d
Jacob Quinn Shenker <jacob@shenker.net>
2020-01-11 12:11:10 -0500

    Added environment.yml, removed -i argument from sed in environment.yml fixer.

--- fix-environment-files.sh
+++ fix-environment-files.sh
@@ -3,7 +3,7 @@ retval=0
 for file in "$@"; do
     if grep -q '^prefix:.*$' $file; then
         retval=1
-        sed -i '' '/^prefix:.*$/d' $file
+        sed -i '/^prefix:.*$/d' $file
     fi
 done
 exit $retval
