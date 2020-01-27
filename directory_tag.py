0440eae83f3b6791d521728284e5c91215d3d618
Jacob Quinn Shenker <jacob@shenker.net>
2020-01-27 17:25:10 -0500

    Second-level subdirectories are now tagged in commit messages.

--- directory_tag.py
+++ directory_tag.py
@@ -5,7 +5,7 @@ import pathlib
 import subprocess
 from collections import Counter
 
-MAX_TAGS = 3
+MAX_TAGS = 2
 
 ret = subprocess.run(
     ["git", "diff-index", "-z", "--cached", "HEAD", "--name-only"], capture_output=True
@@ -15,16 +15,16 @@ if ret.returncode != 0:
     sys.exit(ret.returncode)
 
 files = [pathlib.Path(f.decode()) for f in ret.stdout.split(b"\x00") if f]
-counts = Counter([f.parts[0] for f in files if len(f.parts) >= 2])
-
+counts = Counter([f.parent.parts[:2] for f in files])
+del counts[()]
+if len(counts) > MAX_TAGS:
+    counts = Counter([f.parent.parts[:1] for f in files])
+    del counts[()]
 if not counts or len(counts) > MAX_TAGS:
     sys.exit(0)
-
-tags = [e[0] for e in counts.most_common()]
+tags = ["/".join(e) for e, _ in counts.most_common()]
 
 tag_str = f"[{','.join(tags)}]"
-
-print(tag_str)
 
 with open(sys.argv[1], "r+") as f:
     msg = f.read()
