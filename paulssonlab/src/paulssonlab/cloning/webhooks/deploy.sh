a5c4b5faf388723bda869008ab46da907788a6f0
Jacob Quinn Shenker <jacob@shenker.net>
2020-06-27 14:30:39 -0400

    [cloning/webhooks] Better exit status checking.

--- paulssonlab/src/paulssonlab/cloning/webhooks/deploy.sh
+++ paulssonlab/src/paulssonlab/cloning/webhooks/deploy.sh
@@ -2,12 +2,14 @@
 source="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 tag="gcr.io/paulssonlab/cloning_webhooks"
 if [ "$1" == "-d" ]; then
-    docker build "$source" --tag "$tag"
-    docker push "$tag"
+    docker build "$source" --tag "$tag" \
+        && docker push "$tag"
+    status=$?
 else
     gcloud builds submit "$source" --ignore-file=.dockerignore --gcs-log-dir=gs://paulssonlab_cloudbuild_logs/logs --config=cloudbuild.yaml
+    status=$?
 fi
-if [ $? == 0 ]; then
+if [ $status == 0 ]; then
     gcloud run deploy cloning-webhooks --image "$tag" --allow-unauthenticated --platform managed --region=us-east4
 else
     echo Build failed, aborting.
