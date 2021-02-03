b5bc7a9fa352572cd1cb8a71e48afe2618da516a
Andrew T Martens <andrew_martens@hms.harvard.edu>
2021-02-03 14:38:33 -0500

    [martens/trenchcoat] Small fix.

--- paulssonlab/src/paulssonlab/martens/trenchcoat/trenchcoat.py
+++ paulssonlab/src/paulssonlab/martens/trenchcoat/trenchcoat.py
@@ -318,7 +318,11 @@ def convert(out_dir, in_dir, num_cpu, frames, fovs):
         fovs = range_expand(fovs)
 
     main_conversion_function(
-        hdf_dir=out_dir, nd2_dir=in_dir, num_cpu=num_cpu, frames=frames, fovs=fovs
+        hdf_dir=out_dir,
+        nd2_dir=in_dir,
+        num_cpu=num_cpu,
+        frames=frames,
+        fields_of_view=fovs,
     )
 
 
