e71d0a7c423594531306b14451a71972342b0516
Jacob Quinn Shenker <jacob@shenker.net>
2021-03-15 16:26:55 -0400

    [shenker/sequencing] Snakemake rule to automatically download rerio model, copy guppy barcoding directory.

--- paulssonlab/src/paulssonlab/shenker/sequencing/common.py
+++ paulssonlab/src/paulssonlab/shenker/sequencing/common.py
@@ -1,3 +1,14 @@
+import os
+from pathlib import Path
+
 REMOTE_HOST = "transfer.rc.hms.harvard.edu"
 
 REMOTE_BASE_DIR = "/n/files/SysBio/PAULSSON LAB/Personal Folders"
+
+
+def get_workdir():
+    if "PAULSSONLAB_SCRATCH" in os.environ:
+        subdir = Path.cwd().parent.name
+        return Path(os.environ["PAULSSONLAB_SCRATCH"]) / subdir
+    else:
+        return "."
