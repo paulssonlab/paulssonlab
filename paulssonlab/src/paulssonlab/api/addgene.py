02a6a9cf08e550b8a33932fa15f62e2eaf1df4b4
Jacob Quinn Shenker <jacob@shenker.net>
2022-03-28 09:18:53 -0400

    [api] Addgene API can now parse list of plasmids associated with a publication.

--- paulssonlab/src/paulssonlab/api/addgene.py
+++ paulssonlab/src/paulssonlab/api/addgene.py
@@ -29,7 +29,7 @@ def _get_addgene_plasmid(html):
     # add-to-cart table
     table = parse_html_table(html.find("table.add-to-cart-table", first=True))
     for row in table:
-        del row[""]
+        row.pop("", None)
     if len(table) != 1:
         raise ValueError("expecting a single row in Addgene add-to-cart table")
     # item ("Plasmid")
@@ -135,7 +135,7 @@ def _parse_addgene_well(s):
     return m.groups()
 
 
-def _parse_addgene_kit_row(column_names, tr):
+def _parse_addgene_row(column_names, tr):
     url = None
     tds = tr.find("td")
     for name, td in zip(column_names, tds):
@@ -143,10 +143,15 @@ def _parse_addgene_kit_row(column_names, tr):
         if link is not None and not link.attrs["href"].startswith("#"):
             url = urllib.parse.urljoin(link.base_url, link.attrs["href"])
     row = {name.lower(): td.text for name, td in zip(column_names, tds)}
-    if url is not None:
-        row["url"] = url
+    row["url"] = url
+    return row
+
+
+def _parse_addgene_kit_row(column_names, tr):
+    row = _parse_addgene_row(column_names, tr)
+    if row.get("url"):
         row["catalog"] = int(
-            re.match(r"https?://www.addgene.org/(\d+)/?", url).group(1)
+            re.match(r"https?://www.addgene.org/(\d+)/?", row["url"]).group(1)
         )
     if "well" in row:
         row["well"] = format_well_name(*_parse_addgene_well(row["well"]))
@@ -184,6 +189,26 @@ def _get_addgene(
     catalog = html.find("small#catalog-number", first=True)
     if catalog is None:
         catalog = html.find("span.material-name ~ small", first=True)
+    if catalog is None:
+        # TODO: lab materials tables only load with javascript, so this shows up as an empty table
+        # table = html.find("table#plasmids-datatable", first=True)
+        # plasmids for a publication load statically though
+        table = html.find("table.datatable", first=True)
+        if table is not None:
+            items = parse_html_table(table, row_parser=_parse_addgene_row)
+            for item in items:
+                item.pop("", None)
+            items = _retrieve_sequences(
+                items,
+                include_sequences=include_sequences,
+                include_details=include_details,
+                progress_bar=progress_bar,
+                session=session,
+            )
+            data["items"] = items
+            return data
+        else:
+            raise ValueError("cannot parse Addgene page")
     item, catalog_number = re.search(
         r"\(([\w ]+) #\s*(\d+)\s*\)", catalog.text
     ).groups()
@@ -210,6 +235,33 @@ def _get_addgene(
         raise ValueError(f"unknown Addgene item type: {item}")
 
 
+def _retrieve_sequences(
+    items,
+    include_sequences=True,
+    include_details=False,
+    progress_bar=PROGRESS_BAR,
+    session=None,
+):
+    if (
+        progress_bar is not None
+        and len(items) >= 2
+        and (include_sequences or include_details)
+    ):
+        items_iter = progress_bar(items)
+    else:
+        items_iter = items
+    for item in items_iter:
+        url = item.get("url")
+        if url:
+            if include_sequences:
+                sequence_urls = get_addgene_sequence_urls(url, session=session)
+                item["sequence_urls"] = sequence_urls
+            if include_details:
+                plasmid = get_addgene_plasmid(url, session=session)
+                item.update(plasmid)
+    return items
+
+
 def _get_addgene_kit(
     html,
     include_sequences=True,
@@ -223,20 +275,12 @@ def _get_addgene_kit(
     if table is None:
         raise ValueError("could not find Addgene kit table")
     wells = parse_html_table(table, row_parser=_parse_addgene_kit_row)
-    if (
-        progress_bar is not None
-        and len(wells) >= 2
-        and (include_sequences or include_details)
-    ):
-        wells_iter = progress_bar(wells)
-    else:
-        wells_iter = wells
-    for well in wells_iter:
-        if include_sequences:
-            sequence_urls = get_addgene_sequence_urls(well["url"], session=session)
-            well["sequence_urls"] = sequence_urls
-        if include_details:
-            plasmid = get_addgene_plasmid(well["url"], session=session)
-            well.update(plasmid)
+    wells = _retrieve_sequences(
+        wells,
+        include_sequences=include_sequences,
+        include_details=include_details,
+        progress_bar=progress_bar,
+        session=session,
+    )
     data = {"wells": wells}
     return data
