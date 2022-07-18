5071b707a20628408f830ab6671fed161c863005
Jacob Quinn Shenker <jacob@shenker.net>
2022-07-18 19:09:59 -0400

    [microscope] First cut of autogrid. Should be almost the same as code that was used and tested in a real experiment, but not quite (due to some manual edits on the microscope computer).

---
+++ paulssonlab/src/paulssonlab/microscope/autogrid.py
@@ -0,0 +1,96 @@
+import xml.etree.ElementTree as ET
+import click
+import numpy as np
+
+
+def parse_position(e):
+    return dict(
+        name=e.find("strName").attrib["value"],
+        checked=(e.find("bChecked").attrib["value"].lower() != "false"),
+        x=float(e.find("dXPosition").attrib["value"]),
+        y=float(e.find("dYPosition").attrib["value"]),
+        z=float(e.find("dZPosition").attrib["value"]),
+        pfs_offset=float(e.find("dPFSOffset").attrib["value"]),
+    )
+
+
+def position_element(idx, name, x, y, z, pfs_offset=-1, checked=True):
+    tag = f"Point{idx:05d}"
+    position = ET.Element(tag, runtype="NDSetupMultipointListItem")
+    ET.SubElement(position, "bChecked", runtype="bool", value=str(checked).lower())
+    ET.SubElement(position, "strName", runtype="CLxStringW", value=str(name))
+    ET.SubElement(position, "dXPosition", runtype="double", value=f"{x:.15f}")
+    ET.SubElement(position, "dYPosition", runtype="double", value=f"{y:.15f}")
+    ET.SubElement(position, "dZPosition", runtype="double", value=f"{z:.15f}")
+    ET.SubElement(position, "dPFSOffset", runtype="double", value=f"{pfs_offset:.15f}")
+    return position
+
+
+def export_grid(rows, columns, delta_y, z, pfs_offset, preamble_elements):
+    variant = ET.Element("variant", version="1.0")
+    no_name = ET.SubElement(variant, "no_name", runtype="CLxListVariant")
+    no_name.extend(preamble_elements)
+    forward_row = True
+    idx = 0
+    for row_idx, row_y in enumerate(rows):
+        columns_iter = enumerate(columns)
+        if not forward_row:
+            # enumerate isn't reversible, see https://github.com/cjrh/enumerate_reversible
+            columns_iter = reversed(list(columns_iter))
+        for col_idx, x in columns_iter:
+            name = f"{row_idx:02d}.{col_idx:02d}"
+            y = row_y + col_idx * delta_y
+            no_name.append(position_element(idx, name, x, y, z, pfs_offset))
+            idx += 1
+        forward_row = not forward_row
+    return ET.ElementTree(variant)
+
+
+def autogrid_from_corners(input_xml, x_step, y_step):
+    input_positions = []
+    preamble_elements = []
+    for e in input_xml.findall("./no_name/*"):
+        if e.tag in ("bIncludeZ", "bPFSEnabled"):
+            preamble_elements.append(e)
+        else:
+            input_positions.append(parse_position(e))
+    if len(input_positions) != 4:
+        raise ValueError("expecting four corners")
+    upper_left = input_positions[0]
+    upper_right = input_positions[1]
+    lower_right = input_positions[2]
+    lower_left = input_positions[3]
+    # use z, pfs_offset from upper_left corner position
+    z = upper_left["z"]
+    pfs_offset = upper_left["pfs_offset"]
+    # TODO: use the corners and x_step, y_step to come up with rows, columns (using np.arange)
+    # (this is the actual math part of setting up the grid)
+    rows = np.arange(upper_left["y"], lower_left["y"], -y_step)
+    columns = np.arange(upper_left["x"], upper_right["x"], -x_step)
+    # delta_y is the increment in y every time you move to the next column
+    slope = (upper_right["y"] - upper_left["y"]) / (upper_right["x"] - upper_left["x"])
+    delta_y = -slope * x_step
+    return export_grid(rows, columns, delta_y, z, pfs_offset, preamble_elements)
+
+
+@click.group()
+def cli():
+    pass
+
+
+@cli.command()
+@click.argument("input", type=click.File("r", encoding="utf-16"))
+# @click.argument("output", type=click.File("w", encoding="utf-16"))
+@click.argument("output", type=click.Path())
+@click.option("--x", type=float, required=True)  # these are given in µm
+@click.option("--y", type=float, required=True)
+def from_corners(input, output, x, y):
+    input_xml = ET.parse(input, parser=ET.XMLParser(encoding="utf-16"))
+    output_xml = autogrid_from_corners(input_xml, x, y)
+    # this should write a UTF-16LE with BOM under windows
+    # SEE: https://peter.bloomfield.online/why-python-3-doesnt-write-the-unicode-bom/
+    output_xml.write(output, encoding="utf-16")
+
+
+if __name__ == "__main__":
+    cli()
