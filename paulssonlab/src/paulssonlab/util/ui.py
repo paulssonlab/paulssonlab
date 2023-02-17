9b6ede3a10b8e313dce2a3262c6af7be300e0264
Jacob Quinn Shenker <jacob@shenker.net>
2023-02-17 15:09:03 -0500

    [util] util.ui.display_image shows large images ~8x faster than plt.imshow.

---
+++ paulssonlab/src/paulssonlab/util/ui.py
@@ -0,0 +1,13 @@
+import imageio.v3 as iio
+from IPython.display import Image
+
+
+def display_image(img, format="jpg"):
+    img = (a[0] * 255).astype(np.uint8)
+    if format.lower() in ("jpg", "jpeg"):
+        bytes_ = iio.imwrite("<bytes>", img, extension=".jpeg", quality=95)
+    elif format.lower() == "png":
+        bytes_ = iio.imwrite("<bytes>", img, extension=".png")
+    else:
+        raise ValueError(f"unknown format: {format}")
+    return Image(bytes_)
