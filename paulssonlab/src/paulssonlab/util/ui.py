83507bbc3d9f9722c4140f6d293819b3e6daad65
Jacob Quinn Shenker <jacob@shenker.net>
2023-06-02 11:09:49 -0400

    [util] Add scale kwarg to display_image.

--- paulssonlab/src/paulssonlab/util/ui.py
+++ paulssonlab/src/paulssonlab/util/ui.py
@@ -3,8 +3,11 @@ import numpy as np
 from IPython.display import Image
 
 
-def display_image(img, format="jpg"):
-    img = (img * 255).astype(np.uint8)
+def display_image(img, scale=False, format="jpg"):
+    if scale:
+        img_min = img.min()
+        img = (img - img_min) / (img.max() - img_min)
+    img = (np.clip(img, 0, 1) * 255).astype(np.uint8)
     if format.lower() in ("jpg", "jpeg"):
         bytes_ = iio.imwrite("<bytes>", img, extension=".jpeg", quality=95)
     elif format.lower() == "png":
