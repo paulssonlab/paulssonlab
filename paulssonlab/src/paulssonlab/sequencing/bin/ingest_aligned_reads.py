09b62dfc8f25ed984f11811d9dccb161d4a419da
Jacob Quinn Shenker <jacob@shenker.net>
2023-10-11 11:14:20 -0400

    [sequencing/bin,shenker/sequencing] Fix dx column for dorado 0.4.0, compress arrow output in ingest_aligned_reads.py.

--- paulssonlab/src/paulssonlab/sequencing/bin/ingest_aligned_reads.py
+++ paulssonlab/src/paulssonlab/sequencing/bin/ingest_aligned_reads.py
@@ -4,10 +4,30 @@ from pathlib import Path
 
 import click
 import pyarrow as pa
+import pyarrow.compute as pc
 import pyarrow.parquet as pq
 
 sys.path.append(str(Path(__file__).parents[3]))
 from paulssonlab.sequencing.io import iter_bam_and_gaf
+
+
+# TODO: obsolete when dorado 0.4.1 is released
+def fix_dx(table):
+    name_col = table.column("name")
+    new_dx = pc.if_else(
+        pc.is_in(
+            name_col,
+            pc.list_flatten(
+                pc.split_pattern(
+                    name_col.filter(pc.match_substring(name_col, ";")), ";"
+                )
+            ),
+        ),
+        -1,
+        table.column("dx"),
+    )
+    table = table.set_column(table.column_names.index("dx"), "dx", new_dx)
+    return table
 
 
 def write_bam_and_gaf(
@@ -21,8 +41,13 @@ def write_bam_and_gaf(
     table = pa.Table.from_batches(iter_bam_and_gaf(bam_filename, gaf_filename))
     # can't do this streaming because we need to unify dictionaries
     table = table.unify_dictionaries()
+    table = fix_dx(table)
     if format == "arrow":
-        with pa.ipc.new_file(output_filename, table.schema) as writer:
+        with pa.ipc.new_file(
+            output_filename,
+            table.schema,
+            options=pa.ipc.IpcWriteOptions(compression="lz4"),
+        ) as writer:
             writer.write(table)
     elif format == "parquet":
         pq.write_table(table, output_filename)
